926fb01c865e7bcda7268955c78a036ccbeea035
Maximilian Hedderich <mhedderich@sievers-group.com>
2022-02-16 12:18:39 +0100

    First Commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from crypt import methods
+import flask as Flask, request,jsonify
+
+app = Flask(__name__) 
+
+@app.route('/todo-list', methods=['POST'])
+def postTodoList():
+        return jsonify(request.args)
+
+@app.route('/todo-list', methods=['GET'])
+def postTodoList():
+        return jsonify(request.args)
+
