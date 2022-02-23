38284338e736c3ee8eff817f3f90952cf4fe3bdb
Maximilian Hedderich <mhedderich@sievers-group.com>
2022-02-23 11:53:07 +0100

    First commit

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
-from crypt import methods
-import flask as Flask, request,jsonify
+from flask import Flask, request, jsonify
 
-app = Flask(__name__) 
+app = Flask(__name__)
+todoListsList = {}
+
 
 @app.route('/todo-list', methods=['POST'])
-def postTodoList():
-        return jsonify(request.args)
+def PostTodoList():
+        todoListsList['todo_list'] = request.args
 
-@app.route('/todo-list', methods=['GET'])
-def postTodoList():
-        return jsonify(request.args)
+@app.route('/todo-list/<list_id>', methods=['GET', 'DELETE'])
+def GetTodoList(list_id):
+
+@app.route('/todo-list/<list_id>/entry', methods=['POST'])
+
+@app.route('/todo-list/<list_id>/entry/<entry_id>', methods=['PUT', 'DELETE'])
+
+@app.route('/user', methods=['GET', 'POST'])
+
+@app.route('/user/<user_id>', methods=['DELETE'])
 
